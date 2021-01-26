8a1204ef6cc4c8878e37222fa34f3844b629b137
Denis <denis.andreevir@gmail.com>
2021-01-26 21:27:31 +0300

    create role and user repository

---
+++ src/main/java/ru/andreev/blog/usermanagment/repository/RoleRepository.java
@@ -0,0 +1,13 @@
+package ru.andreev.blog.usermanagment.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ru.andreev.blog.model.entity.Role;
+import ru.andreev.blog.model.enums.ERole;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByRole(ERole role);
+}
