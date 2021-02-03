2fd409656c02f7c9a741a9db87421a6c9e735ccb
Denis <denis.andreevir@gmail.com>
2021-02-03 22:23:20 +0300

    update method saveComment, add check of post validity

--- backend/src/main/java/ru/andreev/blog/postmanagment/exception/PostNotFoundException.java
+++ backend/src/main/java/ru/andreev/blog/postmanagment/exception/PostNotFoundException.java
@@ -1,5 +1,9 @@
 package ru.andreev.blog.postmanagment.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Post not found")
 public class PostNotFoundException extends RuntimeException {
     public PostNotFoundException(Long id) {
         super("Could not find post id: " + id);
--- backend/src/main/java/ru/andreev/blog/postmanagment/service/impl/CommentServiceImpl.java
+++ backend/src/main/java/ru/andreev/blog/postmanagment/service/impl/CommentServiceImpl.java
@@ -8,8 +8,11 @@ import org.springframework.transaction.annotation.Transactional;
 import ru.andreev.blog.domain.dto.request.CommentRequest;
 import ru.andreev.blog.domain.mapper.CommentMapper;
 import ru.andreev.blog.domain.model.entity.Comment;
+import ru.andreev.blog.domain.model.entity.Post;
 import ru.andreev.blog.domain.model.entity.User;
+import ru.andreev.blog.postmanagment.exception.PostNotFoundException;
 import ru.andreev.blog.postmanagment.repository.CommentRepository;
+import ru.andreev.blog.postmanagment.repository.PostRepository;
 import ru.andreev.blog.postmanagment.service.CommentService;
 import ru.andreev.blog.usermanagment.repository.UserRepository;
 
@@ -21,11 +24,13 @@ public class CommentServiceImpl implements CommentService {
 
     private final CommentMapper commentMapper;
     private final UserRepository userRepository;
+    private final PostRepository postRepository;
     private final CommentRepository commentRepository;
 
-    public CommentServiceImpl(CommentMapper commentMapper, UserRepository userRepository, CommentRepository commentRepository) {
+    public CommentServiceImpl(CommentMapper commentMapper, UserRepository userRepository, PostRepository postRepository, CommentRepository commentRepository) {
         this.commentMapper = commentMapper;
         this.userRepository = userRepository;
+        this.postRepository = postRepository;
         this.commentRepository = commentRepository;
     }
 
@@ -35,10 +40,13 @@ public class CommentServiceImpl implements CommentService {
         User user = userRepository.findByUsername(username)
                 .orElseThrow(() -> new UsernameNotFoundException(username));
 
+        Post post = postRepository.findById(Long.valueOf(commentRequest.getPostId()))
+                .orElseThrow(() -> new PostNotFoundException(Long.valueOf(commentRequest.getPostId())));
+
         Comment comment = commentMapper.toEntity(commentRequest);
+        comment.setPost(post);
         comment.setCreatedAt(LocalDateTime.now());
         comment.setCommentator(user);
-
         commentRepository.save(comment);
 
         return new ResponseEntity<>(HttpStatus.CREATED);
