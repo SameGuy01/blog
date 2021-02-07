8e2334775afb5ff0971226f5562b824d22486346
Denis <denis.andreevir@gmail.com>
2021-02-07 21:45:50 +0300

    update exception msg

--- backend/src/main/java/ru/andreev/blog/postmanagment/exception/PostNotFoundException.java
+++ backend/src/main/java/ru/andreev/blog/postmanagment/exception/PostNotFoundException.java
@@ -6,6 +6,6 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Post not found")
 public class PostNotFoundException extends RuntimeException {
     public PostNotFoundException(String detail) {
-        super("Could not find post id: " + detail);
+        super("Could not find post " + detail);
     }
 }
