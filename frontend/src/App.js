2d1d2352d7bb7e4a9a34860a91d82264db1c4640
Denis <denis.andreevir@gmail.com>
2021-02-08 22:44:07 +0300

    install axios, get json data from server

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,44 @@
 import './App.css';
 
 import React from 'react'
-
-class Welcome extends React.Component{
-  render(){
-    return(
-      <div>
-          <h1>Hello, {this.props.someProps} !</h1>
-      </div>
-    )
-  }
-}
+import axios from "axios";
 
 class App extends React.Component{
+
+    state = {
+        data: []
+    }
+
+    componentDidMount() {
+        axios.get('http://localhost:8080/api/v/0/categories')
+            .then(response =>{
+                const data = response.data
+                this.setState({data})
+                console.log(data)
+            })
+    }
+
     render()
     {
         return (
             <div className="App">
-                <Welcome someProps="Blog"/>
+                <h1>Categories</h1>
+                <div>
+                    {this.state.data.map(category => (
+                        <div key={category.id}>
+                            <p>{category.title}</p>
+                            <div>
+                                {category.postList.map( post => (
+                                    <div key={post.id}>
+                                        <p>Id: {post.id}</p>
+                                        <p>Content : {post.content}</p>
+                                        <p>Author : {post.user.username}</p>
+                                    </div>
+                                ))}
+                            </div>
+                        </div>
+                    ))}
+                </div>
             </div>
         );
     }
