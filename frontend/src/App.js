323115f20af71595bed981fe49c7bd06ceb349ab
Denis <denis.andreevir@gmail.com>
2021-02-09 20:34:48 +0300

    add navigation bar, add axios and scss dependencies

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
 
-import React from 'react'
-import axios from "axios";
+import React from 'react';
+
+import NavigationBar from './components/NavigationBar/NavigationBar'
 
 class App extends React.Component{
 
@@ -9,38 +10,12 @@ class App extends React.Component{
         data: []
     }
 
-    componentDidMount() {
-        axios.get('http://localhost:8080/api/v/0/categories')
-            .then(response =>{
-                const data = response.data
-                this.setState({data})
-                console.log(data)
-            })
-    }
-
-    render()
-    {
-        return (
-            <div className="App">
-                <h1>Categories</h1>
-                <div>
-                    {this.state.data.map(category => (
-                        <div key={category.id}>
-                            <p>{category.title}</p>
-                            <div>
-                                {category.postList.map( post => (
-                                    <div key={post.id}>
-                                        <p>Id: {post.id}</p>
-                                        <p>Content : {post.content}</p>
-                                        <p>Author : {post.user.username}</p>
-                                    </div>
-                                ))}
-                            </div>
-                        </div>
-                    ))}
-                </div>
+    render() {
+        return(
+            <div>
+                <NavigationBar/>
             </div>
-        );
+        )
     }
 }
 
