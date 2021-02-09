ff83bb067b73f0c5404e61eee8467c79d05328a6
Denis <denis.andreevir@gmail.com>
2021-02-09 13:34:57 +0300

    clean up

--- backend/src/main/java/ru/andreev/blog/postmanagment/controller/CategoryController.java
+++ backend/src/main/java/ru/andreev/blog/postmanagment/controller/CategoryController.java
@@ -29,9 +29,6 @@ public class CategoryController {
         return categoryService.findById(id);
     }
 
-    @GetMapping("/titles/{title}")
-    public ResponseEntity<?> getByTitle(@PathVariable String title){ return categoryService.findByTitle(title);}
-
     @PostMapping
     @PreAuthorize("hasRole('MODERATOR')")
     public ResponseEntity<?> createNewCategory(@Valid @RequestBody final CategoryRequest categoryRequest){
