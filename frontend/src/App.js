5c0be112630db6fa69a3121c097ba25f8eadf36b
Denis <denis.andreevir@gmail.com>
2021-02-11 16:15:45 +0300

    add login and profile components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,18 +2,46 @@ import './App.css';
 
 import React from 'react';
 
-import NavigationBar from './components/NavigationBar'
+import AuthService from "./services/auth.service"
+
+import Login from "./components/Login";
+import {Route, Switch} from "react-router";
+import Profile from "./components/Profile";
+
 
 class App extends React.Component{
 
-    state = {
-        data: []
+    constructor(props) {
+        super(props);
+        this.logOut = this.logOut.bind(this);
+
+        this.state = {
+            currentUser: undefined
+        };
+    }
+
+    componentDidMount() {
+        const user = AuthService.getCurrentUser();
+
+        if(user) {
+            this.setState({
+                currentUser: user
+            })
+        }
+    }
+
+    logOut(){
+        AuthService.logout();
     }
 
     render() {
+
         return(
             <div>
-                <NavigationBar/>
+                <Switch>
+                    <Route exact path="/login" component={Login} />
+                    <Route exact path="/profile" component={Profile} />
+                </Switch>
             </div>
         )
     }
---
+++ frontend/src/components/Login/index.jsx
@@ -0,0 +1,106 @@
+import React from "react"
+
+import AuthService from "../../services/auth.service"
+
+class Login extends React.Component{
+    constructor(props) {
+        super(props);
+        this.handleLogin = this.handleLogin.bind(this);
+        this.onChangeUsername = this.onChangeUsername.bind(this);
+        this.onChangePassword = this.onChangePassword.bind(this);
+        this.state = {
+            username: "",
+            password: "",
+            loading: false,
+            message: ""
+        };
+    }
+
+    onChangeUsername(e) {
+        this.setState({
+            username: e.target.value
+        });
+    }
+
+    onChangePassword(e) {
+        this.setState({
+            password: e.target.value
+        });
+    }
+
+    handleLogin(e) {
+        e.preventDefault();
+
+        this.setState({
+            message: "",
+            loading: true
+        });
+
+
+            AuthService.login(this.state.username, this.state.password)
+                .then( () => {
+                    this.props.history.push("/profile");
+                    window.location.reload();
+                    },
+                    error => {
+                        const resMessage =
+                            (error.response &&
+                                error.response.data &&
+                                error.response.data.message) ||
+                            error.message ||
+                            error.toString();
+
+                        this.setState({
+                            loading: false,
+                            message: resMessage
+                        });
+                    }
+                );
+
+    }
+
+    render() {
+        return (
+            <div>
+                <form
+                    onSubmit={this.handleLogin}
+                    ref = {c => {
+                        this.form = c;
+                    }}
+                >
+                    <div>
+                        <label htmlFor="username">Username</label>
+                        <input
+                            type="text"
+                            name="username"
+                            value={this.state.username}
+                            onChange={this.onChangeUsername}
+                        />
+                    </div>
+
+                    <div>
+                        <label htmlFor="password">Password</label>
+                        <input
+                            type="password"
+                            name="password"
+                            value={this.state.password}
+                            onChange={this.onChangePassword}
+                        />
+                    </div>
+
+                    <div>
+                        <button
+                            disabled={this.state.loading}>
+                            {this.state.loading && (
+                                <span>Loading</span>
+                            )}
+                            <span>Log in</span>
+                        </button>
+                    </div>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default Login;
