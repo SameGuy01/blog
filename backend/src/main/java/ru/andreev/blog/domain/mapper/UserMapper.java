df666eee9c3ebe2323bbf4cb248ae01ee791d0d2
Denis <denis.andreevir@gmail.com>
2021-02-18 17:51:20 +0300

    rename, add func toDto

--- backend/src/main/java/ru/andreev/blog/domain/mapper/UserMapper.java
+++ backend/src/main/java/ru/andreev/blog/domain/mapper/UserMapper.java
@@ -2,7 +2,8 @@ package ru.andreev.blog.domain.mapper;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
-import ru.andreev.blog.domain.dto.request.UserEditRequest;
+import ru.andreev.blog.domain.dto.request.UserInfoEditRequest;
+import ru.andreev.blog.domain.dto.response.UserInfoResponse;
 import ru.andreev.blog.domain.model.entity.User;
 
 @Component
@@ -14,8 +15,11 @@ public class UserMapper {
         this.modelMapper = modelMapper;
     }
 
-    public User toDto(UserEditRequest dto){
-
+    public User toEntity(UserInfoEditRequest dto){
         return modelMapper.map(dto, User.class);
+    }
+
+    public UserInfoResponse toDto(User entity){
+        return  modelMapper.map(entity, UserInfoResponse.class);
     }
 }
